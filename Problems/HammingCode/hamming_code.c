9c3c0e5939883a40123524f296c35cf3dd88cb25
임병준 <20113316@kookmin.ac.kr>
2016-04-25 22:33:56 +0900

    Hamming Code Solved

---
+++ Problems/HammingCode/hamming_code.c
@@ -0,0 +1,46 @@
+/*
+ * Hamming Code
+ *
+ * Print decoded and recovered hamming code message
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXSIZE 8
+
+void hamming() {
+	char bits[MAXSIZE+1] = "";
+	int i;
+	int syndrome = 0;
+
+	scanf("%s", &bits[1]);
+
+	for (i = 1; i < MAXSIZE; i++) 
+		bits[i] -= 48;
+
+	// 3rd
+	syndrome += bits[4] ^ bits[5] ^ bits[6] ^ bits[7];
+	syndrome = syndrome << 1;
+
+	// 2nd
+	syndrome += bits[2] ^ bits[3] ^ bits[6] ^ bits[7];
+	syndrome = syndrome << 1;
+
+	// 1st
+	syndrome += bits[1] ^ bits[3] ^ bits[5] ^ bits[7];
+
+	bits[syndrome] = !bits[syndrome];
+
+	printf("%d%d%d%d\n", bits[3], bits[5], bits[6], bits[7]);
+}
+
+int main() {
+	int cases;
+
+	scanf("%d", &cases);
+
+	while(cases--)
+		hamming();
+}
