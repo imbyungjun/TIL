1642e083e15413ba5402226456ff6162ad30b353
imbyungjun <20113316@kookmin.ac.kr>
2016-05-11 23:04:53 +0900

    Fix Problem solved

---
+++ Problems/Fix/fix.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void fix() {
+	int i, n, pb, cnt = 0;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n && scanf("%d", &pb); i++)
+		cnt += (i+1==pb) ? 1 : 0;
+
+	printf("%d\n", cnt);
+}
+
+int main() {
+	int cases;
+
+	scanf("%d", &cases);
+
+	while(cases--) 
+		fix();
+}
