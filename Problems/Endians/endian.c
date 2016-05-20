4b0f980ad43c6703af941c37c38333a08ad3708a
imbyungjun <20113316@kookmin.ac.kr>
2016-05-20 20:12:06 +0900

    Endians Problem Solved

---
+++ Problems/Endians/endian.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void invert(char *str) {
+	int i;
+	char tmp;
+
+	for (i = 0; i < 4; i++) {
+		tmp = str[i];
+		str[i] = str[7-i];
+		str[7-i] = tmp;
+	}
+}
+
+void endian() {
+	char str[4][9];
+	char digit[2];
+	unsigned int n, sum = 0;
+	int i, j, cnt = 0;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < 4; i++)
+		strcpy(str[i], "");
+
+	for (i = 0; i < 32; i++, n/=2) {
+		sprintf(digit, "%d", n%2);
+
+		strcat(str[i/8], digit);
+	}
+
+	for (i = 0; i < 4; i++)
+		invert(str[i]);
+
+	for (i = 3; i >= 0; i--) 
+		for (j = 7; j >= 0; j--)
+			sum += (pow(2, cnt++) * (str[i][j] - '0'));
+
+	printf("%ld\n", sum == -1 ? 4294967295 : sum);
+}
+
+int main() {
+	int cases;
+
+	scanf("%d", &cases);
+
+	while (cases--)
+		endian();
+}
