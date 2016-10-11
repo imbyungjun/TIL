240184deda0c4c1a384c7d8461556400cda5196a
imbyungjun <20113316@kookmin.ac.kr>
2016-10-11 10:00:43 +0900

    Escape whitespaces

--- MacOSX/crop.sh
+++ MacOSX/crop.sh
@@ -3,5 +3,5 @@
 # convert command is included in imagemagick package
 
 for i in $1/*.png ; do
-	convert $i -crop 650x416+55+53 ${i%.*}_crop.png
+	convert "$i" -crop 650x416+55+53 "${i%.*}"_crop.png
 done
