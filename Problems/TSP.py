180c3db40bd5643ec06ebab443abf761be65ae48
Imbyungjun <20113316@kookmin.ac.kr>
2018-02-12 14:32:16 +0900

    Update TSP.py
    Set unreachable path's cost to INFINITE

--- Problems/TSP.py
+++ Problems/TSP.py
@@ -18,17 +18,14 @@ def travel(to, visit):
 
     # g(to, null)
     if (1 << N) - 1 == visit:
-        if cost[to][0] == 0:
-            return INF
-        else:
-            return cost[to][0]
+        return cost[to][0]
 
     for i in range(N):
         # visited
         if visit & (1 << i):
             continue
 
-        if cost[to][i] == 0:
+        if cost[to][i] == INF:
             continue
 
         sum = cost[to][i] + travel(i, visit | (1 << i))
@@ -44,5 +41,10 @@ if __name__ == '__main__':
     for i in range(N):
         cost.append([int(n) for n in raw_input().split()])
         dp.append([-1] * (1 << N))
+
+    for i in range(N):
+        for j in range(N):
+            if cost[i][j] == 0:
+                cost[i][j] = INF
 
     print travel(0, 1)
