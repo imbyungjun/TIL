77df99d8c7bc0126753d5eba834c8286e78f7faf
Imbyungjun <20113316@kookmin.ac.kr>
2018-02-12 11:48:44 +0900

    Create TSP.py

---
+++ Problems/TSP.py
@@ -0,0 +1,40 @@
+# 2098
+
+N = int(raw_input())
+cost = []
+dp = []
+INF = 16 * 1000000
+
+
+def travel(to, visit):
+    # g(to, visit:{x, x, x})
+    min = INF
+
+    # print "travel(", to, ",", "{0:b}".format(visit), ")"
+
+    # Already visited
+    if dp[to][visit] >= 0:
+        return dp[to][visit]
+
+    # g(to, null)
+    if (1 << N) - 1 == visit:
+        return cost[to][0]
+
+    for i in range(N):
+        if visit & (1 << i) :
+            continue
+        sum = cost[to][i] + travel(i, visit | (1 << i))
+
+        if sum < min:
+            min = sum
+
+    dp[to][visit] = min
+    return min
+
+
+if __name__ == '__main__':
+    for i in range(N):
+        cost.append([int(n) for n in raw_input().split()])
+        dp.append([-1] * (1 << N+1))
+
+    print travel(0, 1)
